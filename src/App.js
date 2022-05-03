ab4cd8249f5b00bd7dfbd23321bea85ca2427777
yvan yang <yvan.yang@outlook.com>
2022-05-03 16:30:08 +0800

    update code split

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [message, setMessage] = useState();
-  useEffect(() => {
-    async function getMessage() {
-      const _ = await import('./_');
-      const message = _.join(['Hello', 'World'], ' ');
-      setMessage(message);
-    }
-    getMessage();
-  }, []);
+
+  async function getMessage() {
+    const _ = await import('./_');
+    const message = _.join(['Hello', 'World'], ' ');
+    setMessage(message);
+  }
   return (
     <div>
       <h1>Hello World</h1>
+      <button onClick={() => getMessage()}>Get Message</button>
       {message}
     </div>
   );
