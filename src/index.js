ef0a3cbda959f92b7ea77f8e5aa9d8ec7b6939ec
yvan yang <yvan.yang@outlook.com>
2022-05-03 10:42:42 +0800

    add react config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-const greet = "Hello World";
-export default greet;
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
